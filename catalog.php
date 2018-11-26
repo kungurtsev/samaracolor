33a402d0158783fc77118b91963d6ab15985954b
Nikita <kungurtsevni@mail.ru>
2018-11-27 00:35:20 +0400

    First commit samara color

---
+++ catalog.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.css">
+
+    <!--  CSS   -->
+    <link rel="stylesheet" href="assets/css/site.css">
+    <link rel="stylesheet" href="assets/css/catalog.css">
+
+    <title>Каталог товаров</title>
+</head>
+<body>
+<div class="container">
+    <!-- Панель навигации -->
+    <div class="navigation">
+        <div class="navigation-item"><a href="index.php">Главная</a></div>
+        <div class="navigation-item"><a href="catalog.php">Каталог</a></div>
+        <div class="navigation-item"><a href="#">Наши партнеры</a></div>
+    </div>
+    <div class="jumbotron" style="background-color: rgba(130, 99, 99, 0.5);">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="list-group">
+                    <a href="#" class="list-group-item">Краски</a>
+                    <a href="#" class="list-group-item">Кисточки</a>
+                    <a href="#" class="list-group-item">Растворители</a>
+                    <a href="#" class="list-group-item">Остальные товары</a>
+
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="assets/img/green.jpg" alt=""></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Зеленая краска</a>
+                            </p>
+                            <p class="product-desc">Прозводитель KrasHoll</p>
+                            <p class="product-price">Цена: 1.500 рублей</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="assets/img/green.jpg" alt=""></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Синяя краска</a>
+                            </p>
+                            <p class="product-desc">Прозводитель KrasHoll</p>
+                            <p class="product-price">Цена: 1.500 рублей</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="assets/img/green.jpg" alt=""></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Желтая краска</a>
+                            </p>
+                            <p class="product-desc">Прозводитель KrasHoll</p>
+                            <p class="product-price">Цена: 1.450 рублей</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="assets/img/green.jpg" alt=""></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Комплект красок</a>
+                            </p>
+                            <p class="product-desc">Прозводитель KrasHoll</p>
+                            <p class="product-price">Цена: 5000 рублей</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="assets/img/green.jpg" alt=""></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Комплект красок</a>
+                            </p>
+                            <p class="product-desc">Прозводитель KrasHoll</p>
+                            <p class="product-price">Цена: 4.500 рублей</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="product">
+                            <div class="product-img">
+                                <a href="#"><img src="assets/img/green.jpg" alt=""></a>
+                            </div>
+                            <p class="product-title">
+                                <a href="#">Комплект красок</a>
+                            </p>
+                            <p class="product-desc">Прозводитель KrasHoll</p>
+                            <p class="product-price">Цена: 8.500 рублей</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</div>
+</div>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="assets/js/jquery-3.3.1.slim.min.js"></script>
+<script src="assets/js/popper.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.css">
+
+    <!--  CSS   -->
+    <link rel="stylesheet" href="assets/css/site.css">
+
+    <title>Самара краски</title>
+</head>
+<body>
+    <div class="container">
+        <!-- Панель навигации -->
+        <div class="navigation">
+            <div class="navigation-item"><a href="index.php">Главная</a></div>
+            <div class="navigation-item"><a href="catalog.php">Каталог</a></div>
+            <div class="navigation-item"><a href="#">Наши партнеры</a></div>
+        </div>
+
+        <!-- Блок с основным контентом -->
+        <div class="jumbotron" style="background-color: rgba(249, 232, 179, 0.81);">
+            <h1 class="display-4">Добро пожаловать на сайт!</h1>
+            <p class="lead">Приобретайте краски различных видов за небольшую цену в Самаре</p>
+            <hr class="my-4">
+            <p>Мы находимся по адресу Г.Самара Карла Маркса 255.
+            <p>
+                Работая на рынке декоративных покрытий с 1993 года компания ПроКраска знает все о финишных материалах и их производителях. Мы не просто продаем краску и декоративную штукатурку, мы помогаем сделать интерьеры трендовыми, максимально  креативными и в тоже время функциональными и укладывающимися в бюджет.
+                В офисе компании ПроКраска Вы найдёте огромное количество выкрасов декоративных покрытий и штукатурок в разных техниках и стилистических направлениях, которые позволят создать неповторимый интерьер.
+                Мы постоянно развиваемся и расширяем сферы своей деятельности. Поэтому мы всегда открыты для новых деловых предложений и партнёрства.
+            </p>
+            <div class="row">
+                <div class="col-sm-3">
+                    <p>Наш партнер самый крутой на свете. Наш партнер самый крутой на свете.Наш партнер самый крутой на свете.
+                        Наш партнер самый крутой на свете.Наш партнер самый крутой на свете.Наш партнер самый крутой на свете.
+                    </p>
+                </div>
+                <div class="col-sm-3">
+                    <p>
+                        Наш партнер самый крутой на свете. Наш партнер самый крутой на свете.Наш партнер самый крутой на свете.
+                        Наш партнер самый крутой на свете.Наш партнер самый крутой на свете.Наш партнер самый крутой на свете.
+                    </p>
+                </div>
+                <div class="col-sm-3">
+                    <p>
+                        Наш партнер самый крутой на свете. Наш партнер самый крутой на свете.Наш партнер самый крутой на свете.
+                        Наш партнер самый крутой на свете.Наш партнер самый крутой на свете.Наш партнер самый крутой на свете.
+                    </p>
+                </div>
+                <div class="col-sm-3">
+                    <p>
+                        Наш партнер самый крутой на свете. Наш партнер самый крутой на свете.Наш партнер самый крутой на свете.
+                        Наш партнер самый крутой на свете.Наш партнер самый крутой на свете.Наш партнер самый крутой на свете.
+                    </p>
+                </div>
+            </div>
+
+            <!-- Слайдер -->
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="assets/img/slider-1.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="assets/img/slider-2.jpg" alt="Third slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="assets/img/slider-3.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="assets/js/jquery-3.3.1.slim.min.js"></script>
+<script src="assets/js/popper.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
